4d5e8e1d8d66edc47e2c643b21c988c059368e8e
niko peikrishvili <niko.peikrishvili@gmail.com>
2016-06-03 01:37:27 +0400

    removing stuff

--- test/AccountsTest.php
+++ test/AccountsTest.php
@@ -12,7 +12,7 @@ class AccountsTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->account = new Oanda\methods\Account();
+        $this->account = new Oanda\methods\Account('https://api-fxpractice.oanda.com/v1/','70525cdcec08c3f0a0f8a7855ba44cf2-3a24483e03f95b7b2ee9e802d7c5f051');
     }
 
     /**
